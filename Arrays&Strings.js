ac49b012ee672bec058689b78f3115d8d194250e
Cal-Ell <calvin.malagon@ultimagen.com>
2023-06-20 13:15:42 -0700

    arrays and strings update

--- Arrays&Strings.js
+++ Arrays&Strings.js
@@ -10,12 +10,12 @@ const collection = new Map();
 collection.set("Cal", 26);
 collection.set("Nicole", 24);
 
-console.log(collection.get("Cal"));
+//console.log(collection.get("Cal"));
 //The name is the key and the age is the pair, I am getting the key and console logging the value of that key which is age
 
 //Can aso iterate through object
 for(const [key, value] of collection){
-    console.log(`key: ${key}, value: ${value}`);
+    //console.log(`key: ${key}, value: ${value}`);
 }
 
 //Common coding interview question is to create a hash table through class, initial properties should always be size and table
@@ -86,7 +86,7 @@ class HashTable {
     }
     display(){
         this.table.forEach((values, indexes) => {
-            const chainedValues = values.map(([key, values]) => `[${key}, ${values}]`);
+            const chainedValues = values.map(([key, value]) => `[${key}, ${value}]`);
             console.log(`${indexes}: ${chainedValues}`);
         })
     }
@@ -98,4 +98,212 @@ table.set("testing 1", 1);
 table.set("testing 2", 2);
 table.set("testing 3", 3);
 table.set("testing 4", 4);
-table.display();
+table.remove("testing 3");
+//table.display();
+
+// Assignment: Phone Book
+// Description: Create a phone book using a hash table. 
+//Implement functions to add contacts, search for a contact by name, update contact details, and delete a contact.
+// Requirements:
+// The hash table should use the contact's name as the key and store the contact's details as the value.
+// Implement the following functions:
+// addContact(name, phoneNumber): Adds a new contact to the phone book.
+// searchContact(name): Searches for a contact by name and returns the contact's details.
+// updateContact(name, newPhoneNumber): Updates the phone number of an existing contact.
+// deleteContact(name): Deletes a contact from the phone book.
+
+class PhoneBook {
+    constructor(){
+        this.table = new Array(200);
+        this.size = 0;
+    };
+    _hash(key){
+        let hash = 0;
+        for(let i = 0; i < key.length; i++){
+            hash += key.charCodeAt(i);
+        }
+        return hash % this.table.length;
+    };
+    addContact(name, number){
+        const index = this._hash(name);
+        const table = this.table;
+        if(table[index]){
+            for(let i = 0; table[index].length; i++){
+                if(table[index][i][0] === name){
+                    table[index][i][1] = number;
+                    return;
+                }
+                table[index].push([name, number]);
+            }
+        } else {
+            table[index] = [];
+            table[index].push([name, number]);
+        }
+        this.size++;
+    };
+    searchContact(name){
+        const index = this._hash(name);
+        const table = this.table;
+        if(table[index]){
+            for(let i = 0; i < table[index].length; i++){
+                if(table[index][i][0]){
+                    return table[index][i][1];
+                }
+            }
+        } else return undefined;
+    };
+    updateContact(name, newPhoneNumber){
+        const index = this._hash(name);
+        const table = this.table;
+        if(table[index]){
+            for(let i = 0; i < table[index].length; i++){
+                if(table[index][i][0] === name){
+                    table[index][i][1] = newPhoneNumber; 
+                }
+            }
+        } else return undefined;
+    };
+    deleteContact(name){
+        const index = this._hash(name);
+        const table = this.table;
+        if(table[index] && table[index].length){
+            for(let i = 0; i < table.length; i++){
+                if(table[index][i][0] === name){
+                    table[index].splice(i, 1);
+                    this.size--;
+                    return true;
+                }
+            }
+        } else return false
+    }
+    display(){
+        const table = this.table
+        table.forEach((values, indexes) => {
+            const chainedValues = values.map(([key, values]) => `[${key}, ${values}]`);
+            console.log(`${indexes}: ${chainedValues}`)
+        });
+    }
+}
+
+let pb = new PhoneBook();
+pb.addContact('Cal', 305305305);
+pb.addContact('Susan', 907907907);
+pb.addContact('John', 201201201);
+pb.addContact('Alinee', 786786786);
+pb.updateContact('Cal', 205205205);
+pb.deleteContact('Susan');
+//pb.display();
+
+
+
+// Assignment: Word Frequency Counter
+// Description: Implement a word frequency counter using a hash table. 
+//Create a function that takes a string as input and returns a hash table containing each unique word and its frequency.
+// Requirements:
+// The hash table should use the word as the key and store the frequency as the value.
+// Ignore case sensitivity and punctuation while counting word frequencies.
+// Implement the following function:
+// countWordFrequency(text): Takes a string text as input and returns a hash table with word frequencies.
+
+
+
+
+
+
+class WordCounter {
+    constructor() {
+        this.table = new Array(200);
+        this.size = 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Assignment: Cache Implementation
+// Description: Implement a cache using a hash table to store key-value pairs. 
+//The cache should have a maximum capacity, and if the cache exceeds the capacity, the least recently used item should be evicted.
+// Requirements:
+// The hash table should use the key as the hash key and store the corresponding value.
+// Implement the following functions:
+// get(key): Retrieves the value associated with the given key from the cache. If the key is not found, return null.
+// put(key, value): Inserts a new key-value pair into the cache. If the cache exceeds its capacity, evict the least recently used item before inserting the new pair.
+
+class CacheHash {
+    constructor(){
+        this.table = new Array(5);
+        this.size = 0;
+    }
+    _hash(key){
+        let hash = 0;
+        for(let i = 0; i < key.length; i++){
+            hash += key.charCodeAt(i);
+        }
+        return hash % this.table.length;
+    };
+    get(key) {
+        const index = this._hash(key);
+        const table = this.table;
+        if (table[index]) {
+            for (let i = 0; i < table[index].length; i++) {
+                if (table[index][i] && table[index][i][0] === key) {
+                    return table[index][i][1];
+                }
+            }
+        }
+        return null;
+    };
+    put(key, value){
+        const index = this._hash(key);
+        const table = this.table;
+        if(this.size === 5) table.shift();
+        if(table[index]){
+            for(let i = 0; i < table[index].length; i++){
+                if(table[index][i][0] && table[index][i][0] === key){
+                    table[index][i][1] = value;
+                    return;
+                }
+                table[index].push([key, value]);
+            }
+        } else {
+            table[index] = [];
+            table[index].push([key, value]);
+        }
+        this.size++;
+    };
+    display(){
+        this.table.forEach((values, indexes) => {
+            const chainedValues = values.map(([key, value]) => `[${key}, ${value}]`);
+            console.log(`${indexes}: ${chainedValues}`);
+        });
+    }
+};
+
+const c = new CacheHash();
+c.put("Cache 1", 1);
+c.put("Cache 2", 2);
+c.put("Cache 3", 3);
+//c.get("Cache 1");
+c.put("Cache 4", 4);
+c.put("Cache 5", 5);
+c.put("Cache 6", 6);
+console.log(c.get("Cache 1"));
+console.log(c.get("Cache 2"));
+console.log(c.get("Cache 3"));
+console.log(c.get("Cache 4"));
+console.log(c.get("Cache 5"));
+console.log(c.get("Cache 6"));
+c.display();
