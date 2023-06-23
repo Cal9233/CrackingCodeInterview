a4a06e44a93980070a41972f6236cc31f2d0be47
Cal-Ell <calvin.malagon@ultimagen.com>
2023-06-23 16:51:50 -0700

    part 1

--- Algorithms/Traversal.js
+++ Algorithms/Traversal.js
@@ -1,3 +1,31 @@
 //Breadth-First Search (BFS)
+//Explores all vertices in a graph  
+//*vertices: elements/objects in a graph connected by edges
+
+//BFS searchs all vertices before moving to the next level
+//BFS is typically implemented in queues.
+
+class visitedVertices {
+    constructor() {
+        this.table = null;
+        this.size = 0;
+    }
+
+    _hash(key) {
+        let index = 0;
+
+        for(let i = 0; i < key.length; i++){
+            
+        }
+    }
+}
+
+const bfs = (value) => {
+    //empty queue to store vertices it visited
+    let queue = [];
+
+    //create hashset or boolean array to keep track of visited vertices
+
+};
 
 //Depth-First Search (DFS)
